914e6ea378d000a529c7cd4272cc4cdf17e2f243
navid <navid@navid.com>
2017-07-20 23:10:32 +0200

    adding some aliases

---
+++ myrc.sh
@@ -0,0 +1,19 @@
+
+
+
+#-------------------- Directory
+alias cd..='cd ..'
+alias ..='cd ..'
+alias .2='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+alias mkdir='mkdir -pv'
+alias back='cd "$OLDPWD"'
+#alias back="cd -"
+
+
+
+
+#-------------------- Miscellaneous
+alias ppjson='python -m json.tool'
