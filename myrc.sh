ddeff80cc13bdf85a3684211770b4f90b8634c8c
hawkig8b <hawkig8b@gmail.com>
2017-07-24 10:08:26 +0200

    syncing myrc.sh

--- myrc.sh
+++ myrc.sh
@@ -99,3 +99,25 @@ function install_apps {
     fi
   done
 }
+
+
+
+#====================== Betty
+export BETTY_HOME="/home/nvd/betty"
+alias gobetty="cd $BETTY_HOME"
+alias goui="cd $BETTY_HOME/betty_orderfulfillment_ui"
+alias gosecrets="cd $BETTY_HOME/betty_secrets"
+alias gopublicapi="cd $BETTY_HOME/betty_orderfulfillment_publicapi"
+alias goorderservice="cd $BETTY_HOME/betty_orderfulfillment_orderservice"
+alias gobuildsupport="cd $BETTY_HOME/betty_build_support"
+alias gokubernetes="cd $BETTY_HOME/betty_orderfulfillment_kubernetes"
+alias godockercompose="cd $BETTY_HOME/betty_build_support/etc/docker-compose"
+
+alias dockerup="cd $BETTY_HOME/betty_build_support/etc/docker-compose && docker-compose up -d"
+export CQLSH_NO_BUNDLED=true
+alias bettyup="cd $BETTY_HOME/betty_build_support/etc/docker-compose && docker-compose up -d"
+alias bettyUiDown="docker ps | grep -C 0 orderfulfillment/ui:DEV |  tee /dev/tty  | head -1 | awk '{print $1;}' | xargs docker stop"
+alias bettyStopLocalUI="docker ps | grep -C 0 build | tee /dev/tty  | awk '{print $1;}' | xargs docker stop"
+alias bettyStartLocalUI="cd $BETTY_HOME/betty_orderfulfillment_ui/ui/ && ./docker-gulp.sh serve"
+alias updatecontainers="cd /home/nvd/fixstuff/betty_build_support && git pull --rebase &&
+cd /home/nvd/fixstuff/betty_build_support/etc/docker-compose && docker-compose pull"
