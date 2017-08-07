0d01aaaf9362a29bc87715516eeaca63a3898e2d
hawkig8b <hawkig8b@gmail.com>
2017-08-07 18:09:41 +0200

    syncing myrc.sh

--- myrc.sh
+++ myrc.sh
@@ -65,6 +65,7 @@ atom #advanced text editor
 cowsay # show a cool cow message
 curl
 git
+git-core
 gitk
 gitg
 glipper #multi-item clipboard utility
@@ -80,6 +81,7 @@ tmux #termial tool
 tree #print directory tree structure
 uget #a gui downlaod manager
 unp #cl unzip utility
+wget
 xclip #cl clipboard utility
 
 )
@@ -87,10 +89,17 @@ xclip #cl clipboard utility
 GREEN_CHECK_SIGN="\e[0;32m\xE2\x9C\x94\e[0m"
 RED_CROSS_SIGN="\e[0;31m\xe2\x9c\x98\e[0m"
 
+function install_third_party_keys {
+  echo "Adding 3rd party repositories ..."
+  sudo add-apt-repository ppa:webupd8team/atom
+}
+
 function install_apps {
   rm -f /tmp/install_apps_out.log tmp/install_apps_err.log
-  echo "Updating the apt index"
+
+  echo "Updating the apt index ..."
   sudo apt-get update >>/tmp/install_apps_out.log 2>>/tmp/install_apps_err.log
+
   for item in $install_list ;do
     echo -n "installing" $item "... "
     sudo apt-get install $item -y  >>/tmp/install_apps_out.log 2>>/tmp/install_apps_err.log
