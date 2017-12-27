ab068b9f09acea3dddc306e5b9418c0331c9c040
hawkig8b <hawkig8b@gmail.com>
2017-12-27 23:15:59 +0100

    added jsonviewer script which can be invoked using a shortcut

---
+++ viewjson.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+#add to keyboard shortcuts : /usr/bin/zsh /home/nvd/.nvdsh/viewjson.sh
+
+alias pbcopy="xclip -selection c"
+alias pbpaste="xclip -selection clipboard -o"
+alias ppjson='python -m json.tool'
+alias viewjson='TEMPJSON=$(mktemp).json && pbpaste | ppjson > $TEMPJSON && xdg-open $TEMPJSON'
+viewjson
