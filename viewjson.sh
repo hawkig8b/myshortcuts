d669e9e5358f9286194b3229a26464fe6a0d199e
hawkig8b <hawkig8b@gmail.com>
2017-12-28 10:22:46 +0100

    adding date label to json tmp file

--- viewjson.sh
+++ viewjson.sh
@@ -5,7 +5,7 @@
 alias pbcopy="xclip -selection c"
 alias pbpaste="xclip -selection clipboard -o"
 alias ppjson='python -m json.tool'
-alias viewjson='TEMPJSON=$(mktemp).json && pbpaste | ppjson > $TEMPJSON && xdg-open $TEMPJSON'
+alias viewjson='TEMPJSON=$(mktemp)$(echo "_")$(date +"%y%m%d-%H%M%S").json && pbpaste | ppjson > $TEMPJSON && xdg-open $TEMPJSON'
 
 viewjson
 
