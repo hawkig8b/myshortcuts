9c70f3ef678ba45feb3f5db11a7968c100f76ae7
hawkig8b <hawkig8b@gmail.com>
2017-12-28 01:27:50 +0100

    adding error for json view

--- viewjson.sh
+++ viewjson.sh
@@ -6,4 +6,10 @@ alias pbcopy="xclip -selection c"
 alias pbpaste="xclip -selection clipboard -o"
 alias ppjson='python -m json.tool'
 alias viewjson='TEMPJSON=$(mktemp).json && pbpaste | ppjson > $TEMPJSON && xdg-open $TEMPJSON'
+
 viewjson
+
+[[ $? -eq 0  ]] || zenity \
+--error \
+--text="Error Processing Json" \
+--title="ViewJson" \
